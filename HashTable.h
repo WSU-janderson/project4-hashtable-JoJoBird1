73da52097ca1c73e73b7869ac1f992b7a699984f
Jordan Wilson <wilson.1114@wright.edu>
2025-11-03 13:34:31 -0500

    adding to .h file and the classes

--- HashTable.h
+++ HashTable.h
@@ -1,3 +1,50 @@
 /**
  * HashTable.h
  */
+
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+/**
+ * Class - BucketType
+ */
+enum class BucketType {NORMAL, ESS, EAR};
+
+/**
+ * Class - HashTableBucket
+ */
+class HashTableBucket {
+// default constructor
+HashTableBucket();
+
+// parameterized constructor
+HashTableBucket(const string& key, const size_t& value);
+};
+
+/**
+ * Class - HashTable
+ */
+class HashTable {
+// Private Variables
+private:
+    std::vector<HashTableBucket> table;
+    size_t tCapacity;
+    size_t count;
+
+// Public Variables
+public:
+    HashTable (size_t initCapacity = 8);
+    bool insert(const std::string& key, const size_t& value);
+    bool remove(const std::string& key);
+    bool contains(const string& key) const;
+    optional<size_t> get(const string& key) const;
+    size_t& operator[](const string& key);
+    vector<std::string> keys() const;
+    double alpha() const;
+    size_t capacity() const;
+    size_t size() const;
+
+
+};
+
+#endif
