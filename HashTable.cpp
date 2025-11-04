123b7053c1a3ac62d0c5a727bb85bc10d28df8d3
Jordan Wilson <wilson.1114@wright.edu>
2025-11-03 21:32:32 -0500

    commenting formatting

--- HashTable.cpp
+++ HashTable.cpp
@@ -33,12 +33,12 @@ bool HashTable::insert(const std::string& key, const size_t& value) {
  if (alpha() >= 0.5) {
   resize();
  }
- size_t index = hash<std::string>{} (key) % tCapacity;
+ size_t index = hash<std::string>{} (key) % tCapacity;               // home bucket using built-in string hash
 // for loop iterate through hash
  for (size_t i = 0; i < tCapacity; i++) {
-  size_t pIndex = (index + i) % tCapacity;
+  size_t pIndex = (index + i) % tCapacity;                          // next probe position
 
-  HashTableBucket& bucket = table[pIndex];
+  HashTableBucket& bucket = table[pIndex];                          // grab reference to that bucket
 // duplicate key was found
   if (bucket.isNormal() && bucket.getKey() == key) {
    return false;
@@ -59,23 +59,23 @@ bool HashTable::insert(const std::string& key, const size_t& value) {
  * Marking a bucket as empty after removed.
 */
 bool HashTable::remove(const std::string& key) {
- size_t index = hash<std::string>{}(key) % tCapacity;
+ size_t index = hash<std::string>{}(key) % tCapacity;          // home bucket using built-in string hash
 // for loop iterate through hash
  for (size_t i = 0; i < tCapacity; i++) {
-  size_t pIndex = (index + i) % tCapacity;
-  HashTableBucket& bucket = table[pIndex];
+  size_t pIndex = (index + i) % tCapacity;                    // next probe position
+  HashTableBucket& bucket = table[pIndex];                    // grab reference to that bucket
 // if condition to check if spot was never used
   if (bucket.isEmptySinceStart()) {
    return false;
   }
 // if condition found key
   if (bucket.isNormal() && bucket.getKey() == key) {
-   bucket.makeEar();
+   bucket.makeEar();                                       // mark as "empty after removal"
    count--;
    return true;
   }
  }
- return false;
+ return false;                                             // key not found
 }
 
 /**
@@ -84,11 +84,11 @@ bool HashTable::remove(const std::string& key) {
  * Otherwise, will return false
 */
 bool HashTable::contains(const string& key) const {
- size_t index = hash<std::string>{}(key) % tCapacity;
+ size_t index = hash<std::string>{}(key) % tCapacity;                  // home bucket using built-in string hash
 // for loop iterate through hash
  for (size_t i = 0; i < tCapacity; i++) {
-  size_t pIndex = (index + 1) % tCapacity;
-  const HashTableBucket& bucket = table[pIndex];
+  size_t pIndex = (index + 1) % tCapacity;                             // next probe position
+  const HashTableBucket& bucket = table[pIndex];                       // grab reference to that bucket
 //if condition to check if spot was never used
   if (bucket.isEmptySinceStart()) {
    return false;
@@ -98,7 +98,7 @@ bool HashTable::contains(const string& key) const {
    return true;
   }
  }
- return false;
+ return false;                                              // key not found
 }
 
 /**
@@ -108,21 +108,21 @@ bool HashTable::contains(const string& key) const {
 * nullopt.
 */
 std::optional<size_t> HashTable::get(const string& key) const {
- size_t index = hash<std::string>{}(key) % tCapacity;
+ size_t index = hash<std::string>{}(key) % tCapacity;                // home bucket using built-in string hash
 // for loop iterate through hash
  for (size_t i = 0; i < tCapacity; i++) {
-  size_t pIndex = (index +1) % tCapacity;
-  const HashTableBucket& bucket = table[pIndex];
-// if condition to check if spot was never used
+  size_t pIndex = (index +1) % tCapacity;                            // next probe position
+  const HashTableBucket& bucket = table[pIndex];                     // grab reference to that bucket
+// if condition to check if reached empty slot
   if (bucket.isEmptySinceStart()) {
    return std::nullopt;
   }
 // if condition to check if key was found
   if (bucket.isNormal() && bucket.getKey() == key) {
-   return bucket.getValue();
+   return bucket.getValue();                       // return value at specified key
   }
  }
- return std::nullopt;
+ return std::nullopt;                              // key not found
 }
 
 /**
@@ -136,14 +136,14 @@ std::optional<size_t> HashTable::get(const string& key) const {
 * to access keys not in the table inside the bracket operator method.
 */
 size_t& HashTable::operator[](const string& key) {
- size_t index = hash<std::string>{}(key) % tCapacity;
+ size_t index = hash<std::string>{}(key) % tCapacity;                // home bucket using built-in string hash
 // for loop to iterate through hash
  for (size_t i = 0; i < tCapacity; i++) {
-  size_t pIndex = (index + i) % tCapacity;
-  HashTableBucket& bucket = table[pIndex];
+  size_t pIndex = (index + i) % tCapacity;                           // next probe position
+  HashTableBucket& bucket = table[pIndex];                           // grab reference to that bucket
 // if condition to check key found
   if (bucket.isNormal() && bucket.getKey() == key) {
-   return bucket.getValue();
+   return bucket.getValue();                                        // return value at specified key
   }
  }
  throw runtime_error("Key not found");
@@ -155,13 +155,14 @@ size_t& HashTable::operator[](const string& key) {
 */
 std::vector<std::string> HashTable::keys() const {
  vector<string> allKeys;
-
+// for loop
  for (const auto& bucket : table) {
+// if condition
   if (bucket.isNormal()) {
    allKeys.push_back(bucket.getKey());
   }
  }
- return allKeys;
+ return allKeys;                               // returns keys in table
 }
 
 /**
@@ -198,24 +199,25 @@ size_t HashTable::size() const {
  * resizes the hash-table
  */
 void HashTable::resize() {
- size_t newCapacity = tCapacity * 2;
- vector<HashTableBucket> newTable(newCapacity);
-
+ size_t newCapacity = tCapacity * 2;                // sets new capacity
+ vector<HashTableBucket> newTable(newCapacity);     // creates new table with updated capacity
+// for loop
  for (auto& bucket : table) {
-  if (bucket.isNormal()) {
-   size_t index = hash<std::string>{}(bucket.getKey()) % newCapacity;
-
+  if (bucket.isNormal()) {                          // if condition for a key-value pair
+   size_t index = hash<std::string>{}(bucket.getKey()) % newCapacity;     // new hash index on updated capacity
+// for loop for linear probing
    for (size_t i = 0; i < newCapacity; i ++) {
-    size_t pIndex = (index + i) % newCapacity;
+    size_t pIndex = (index + i) % newCapacity;      // probe index calculation
+// if condition to check if it's empty
     if (newTable[pIndex].isEmpty()) {
-     newTable[pIndex].load(bucket.getKey(), bucket.getValue());
-     break;
+     newTable[pIndex].load(bucket.getKey(), bucket.getValue());      // inserts the key-value pair
+     break;    // break to the next bucket
     }
    }
   }
  }
- table = move(newTable);
- tCapacity = newCapacity;
+ table = move(newTable);        // replace old table with the new updated table
+ tCapacity = newCapacity;           // updates the new capacity
 }
 
 
@@ -224,14 +226,16 @@ void HashTable::resize() {
  * prints out hash-table
  */
 ostream& operator<<(ostream& os, const HashTable& ht1) {
+ // for loop iterate through table
  for (size_t i = 0; i < ht1.tCapacity; i++) {
   const HashTableBucket& bucket = ht1.table[i];
-
+// if condition checks for a key-value pair
   if (bucket.isNormal()) {
+   // print statement
    os << "Bucket " << i << ": " << bucket.getKey() << ", " << bucket.getValue() << endl;
   }
  }
- return os;
+ return os;        // return print statement
 }
 
 /**
@@ -239,8 +243,10 @@ ostream& operator<<(ostream& os, const HashTable& ht1) {
  * prints out single bucket
  */
 ostream& operator<<(ostream& os, const HashTableBucket& bucket) {
+ // if condition check for a key-value pair
  if (bucket.isNormal()) {
+  // print statement
   os << "< " << bucket.getKey() << ", " << bucket.getValue() << " >" << endl;
  }
- return os;
+ return os;      // return print statement
 }
